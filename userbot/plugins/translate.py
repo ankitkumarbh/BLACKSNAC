f8362f1b06796217ef4f9ff41b9f56068c2500ac
BLACKSNAC <77516086+BLACKSNAC@users.noreply.github.com>
2021-01-21 15:05:11 +0530

    Update translate.py

--- userbot/plugins/translate.py
+++ userbot/plugins/translate.py
@@ -36,7 +36,7 @@ async def _(event):
         after_tr_text = translated.text
         # TODO: emojify the :
         # either here, or before translation
-        output_str = """**Translated By 𝔻𝔸ℝ𝕂 ℂ𝕆𝔹ℝ𝔸** from {} to {}
+        output_str = """**Translated By 𝔹𝕃𝔸ℂ𝕂 𝕊ℕ𝔸𝕂𝔼** from {} to {}
 {}""".format(
             translated.src,
             lan,
